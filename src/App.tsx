3c0f2c7526c594c0d349101366caac90b210762a
syria_hamster <the.syria.hamster@gmail.com>
2021-03-17 21:54:22 +0900

    Add Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Button, ButtonToolbar } from 'react-bootstrap';
+import React from "react";
+import { Button, ButtonToolbar } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <ButtonToolbar>
-    <Button >Primary</Button>
-</ButtonToolbar>
+        <Button>Primary</Button>
+      </ButtonToolbar>
     </div>
   );
 }
